ca2e9549819194fa4f2062f7d4f6033e85cc82e7
aymane-sebai <aymansebai05@gmail.com>
2025-04-02 04:01:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-gray-900 text-white min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
