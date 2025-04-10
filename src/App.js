d478a64daad2daa118bb4cace203ff26ebd6bb26
aymane-sebai <aymansebai05@gmail.com>
2025-04-10 02:30:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import MovieDetails from "./pages/MovieDetails";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div className="bg-gray-900 text-white min-h-screen">
+      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-        </Routes>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
 }
 
 export default App;
-
