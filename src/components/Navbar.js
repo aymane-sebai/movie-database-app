7d4604a855a9c561a83d9879604cf9217ef4dc31
aymane-sebai <aymansebai05@gmail.com>
2025-04-10 02:32:54 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 shadow-md">
+    <nav className="bg-gray-800 p-4 shadow">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">
-          🎬 MovieDB
-        </Link>
+        <Link to="/" className="text-xl font-bold">ASMMovies</Link>
+        <div className="space-x-4">
+          <Link to="/" className="hover:underline">Home</Link>
+          <Link to="#" className="hover:underline">About Me</Link>
+        </div>
       </div>
     </nav>
   );
