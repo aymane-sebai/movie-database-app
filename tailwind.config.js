25b04d00e99d4824979b32acf20a27008ba54a28
aymane-sebai <aymansebai05@gmail.com>
2025-04-12 16:55:22 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#1e293b',
-        secondary: '#64748b',
-        accent: '#fbbf24'
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1e293b",
+          secondary: "#64748b",
+          accent: "#fbbf24",
+        },
+        fontFamily: {
+          sans: ["Inter", "sans-serif"],
+        },
       },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif']
-      }
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
+  
