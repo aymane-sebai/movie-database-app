b9498efecf33e065c206ba26db5f39b17681a1e8
aymane-sebai <aymansebai05@gmail.com>
2025-04-12 16:57:23 +0200

    Update and rename Footer.js to Footer.jsx

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-800 text-center p-4 mt-10">
-      <p>©2025 ASMMovies — Follow Me</p>
-    </footer>
-  );
-}
-
-export default Footer;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer className="bg-gray-800 text-center p-4 mt-10">
+        <p>©2025 ASMMovies — Follow Me</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
