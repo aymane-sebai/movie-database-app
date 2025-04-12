f88ca5d20bd00ce65656bcd2791b5985edb7e8e0
aymane-sebai <aymansebai05@gmail.com>
2025-04-12 16:50:31 +0200

    Update and rename App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import MovieDetails from './pages/MovieDetails';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import MovieDetails from './pages/MovieDetails.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
